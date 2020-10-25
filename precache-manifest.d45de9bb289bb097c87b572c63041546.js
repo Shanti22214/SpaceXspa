5847a9fd022bb887ba5748d973b2b790251d8f00
Vladislav Rykovskiy <shantyo22214@gmail.com>
2020-10-25 14:44:43 +0300

    Add project

---
+++ precache-manifest.d45de9bb289bb097c87b572c63041546.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c960ae3cac42fdb494bf79972758276b",
+    "url": "/SpaceXspa/index.html"
+  },
+  {
+    "revision": "81e562a7360d81e9495e",
+    "url": "/SpaceXspa/static/css/main.4c0a9d38.chunk.css"
+  },
+  {
+    "revision": "66048008a4c28571854a",
+    "url": "/SpaceXspa/static/js/2.4b01b675.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpaceXspa/static/js/2.4b01b675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e562a7360d81e9495e",
+    "url": "/SpaceXspa/static/js/main.7d30018a.chunk.js"
+  },
+  {
+    "revision": "2e0651c7385dc03c5d9b",
+    "url": "/SpaceXspa/static/js/runtime-main.4c3e097a.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/SpaceXspa/static/media/logo.d5ab31ce.svg"
+  }
+]);
