9bfe1d8b21c3285da595e2f952a3befe8b0973f8
Vladislav Rykovskiy <shantyo22214@gmail.com>
2020-11-13 15:00:38 +0300

    fix footer

---
+++ precache-manifest.8924ee8267c597b4e320fcb76ceabe8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e240ec9ede7a7f57a58258edee748896",
+    "url": "/SpaceXspa/index.html"
+  },
+  {
+    "revision": "e836b9ef2f1b727de0a0",
+    "url": "/SpaceXspa/static/css/main.4c0a9d38.chunk.css"
+  },
+  {
+    "revision": "66048008a4c28571854a",
+    "url": "/SpaceXspa/static/js/2.4b01b675.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpaceXspa/static/js/2.4b01b675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e836b9ef2f1b727de0a0",
+    "url": "/SpaceXspa/static/js/main.be73a26e.chunk.js"
+  },
+  {
+    "revision": "2e0651c7385dc03c5d9b",
+    "url": "/SpaceXspa/static/js/runtime-main.4c3e097a.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/SpaceXspa/static/media/logo.d5ab31ce.svg"
+  }
+]);
